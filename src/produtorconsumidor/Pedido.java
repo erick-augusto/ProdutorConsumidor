4d4ae6f4f4b665eb0c4aa7ea59928b9be9828322
erick-augusto <erick-augusto@users.noreply.github.com>
2018-03-22 15:12:18 -0300

    Classe Pedido

---
+++ src/produtorconsumidor/Pedido.java
@@ -0,0 +1,14 @@
+package produtorconsumidor;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Erick
+ */
+public class Pedido {
+    int numero;
+    String descricao;
+    static int count = 0;
+    static Random ran = new Random();
+}
