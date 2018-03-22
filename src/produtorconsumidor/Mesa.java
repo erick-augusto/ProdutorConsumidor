0e77e35a5bbce26c086462536ebf057119ecedb9
erick-augusto <erick-augusto@users.noreply.github.com>
2018-03-22 15:38:34 -0300

    Mesa Construtor

--- src/produtorconsumidor/Mesa.java
+++ src/produtorconsumidor/Mesa.java
@@ -10,4 +10,8 @@ public class Mesa extends Thread {
     BlockingQueue<Pedido> fila;
     int numero;
     
+    public Mesa(int numero, BlockingQueue<Pedido> fila){
+        this.numero = numero;
+        this.fila = fila;
+    }
 }
