3947beb8351bdb3f59aa359b381b7c32aaa1e5c3
erick-augusto <erick-augusto@users.noreply.github.com>
2018-03-22 15:23:45 -0300

    Construtor Pedido

--- src/produtorconsumidor/Pedido.java
+++ src/produtorconsumidor/Pedido.java
@@ -11,4 +11,10 @@ public class Pedido {
     String descricao;
     static int count = 0;
     static Random ran = new Random();
+    
+    public Pedido(){
+        String opcoes[] = {"opção 1","opção 2","opção 3","opção 4","opção 5"};
+        numero = ++count;
+        descricao = opcoes[ran.nextInt(opcoes.length)];
+    }
 }
