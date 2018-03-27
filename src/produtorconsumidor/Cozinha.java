a638c537e2103cc8de251aaf0a34e1839db980a9
erick-augusto <erick-augusto@users.noreply.github.com>
2018-03-27 17:49:10 -0300

    Classe Cozinha

---
+++ src/produtorconsumidor/Cozinha.java
@@ -0,0 +1,15 @@
+package produtorconsumidor;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ *
+ * @author Erick
+ */
+public class Cozinha extends Thread {
+    BlockingQueue<Pedido> fila;
+    
+    public Cozinha(BlockingQueue<Pedido> fila){
+        this.fila = fila;
+    }
+}
