68bfb37afc90bd774b2a9ce97aac2279722e47f1
erick-augusto <erick-augusto@users.noreply.github.com>
2018-03-28 16:47:12 -0300

    Inserindo Mesas

--- src/produtorconsumidor/ProdutorConsumidor.java
+++ src/produtorconsumidor/ProdutorConsumidor.java
@@ -11,6 +11,12 @@ public class ProdutorConsumidor {
 
     public static void main(String[] args) {
         BlockingQueue<Pedido> pedidos = new ArrayBlockingQueue<Pedido>(100);
+        
+        Mesa m1 = new Mesa(1,pedidos);
+        Mesa m2 = new Mesa(2,pedidos);
+        Mesa m3 = new Mesa(3,pedidos);
+        Mesa m4 = new Mesa(4,pedidos);
+        Mesa m5 = new Mesa(5,pedidos);
     }
     
 }
