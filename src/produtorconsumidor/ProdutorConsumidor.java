2cf11f5f20a242350789b59627e9d6e8c167d8f6
erick-augusto <erick-augusto@users.noreply.github.com>
2018-04-05 16:06:20 -0300

    Comentando mesas

--- src/produtorconsumidor/ProdutorConsumidor.java
+++ src/produtorconsumidor/ProdutorConsumidor.java
@@ -23,20 +23,20 @@ public class ProdutorConsumidor {
         for(int i = 0; i < num_mesas; i++){
             vet[i].start();
         }
-        Mesa m1 = new Mesa(1,pedidos);
+        /*Mesa m1 = new Mesa(1,pedidos);
         Mesa m2 = new Mesa(2,pedidos);
         Mesa m3 = new Mesa(3,pedidos);
         Mesa m4 = new Mesa(4,pedidos);
-        Mesa m5 = new Mesa(5,pedidos);
+        Mesa m5 = new Mesa(5,pedidos);*/
         
         Cozinha c1 = new Cozinha(pedidos);
         Cozinha c2 = new Cozinha(pedidos);
         
-        m1.start();
+        /*m1.start();
         m2.start();
         m3.start();
         m4.start();
-        m5.start();
+        m5.start();*/
         
         c1.start();
         c2.start();
